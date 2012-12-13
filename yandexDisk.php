c846550e0c6b35d3dd18df1554d2903fb86f7018
forpdfsending@gmail.com <forpdfsending@gmail.com>
2012-12-13 09:56:02 +0400

    initial commit

---
+++ yandexDisk.php
@@ -0,0 +1,39 @@
+<?php
+
+
+error_reporting(0);
+
+if (!class_exists('webdav_client')) {
+ require('./class_webdav_client.php');
+} 
+
+$wdc = new webdav_client();
+$wdc->set_server('ssl://webdav.yandex.ru');
+$wdc->set_port(443);
+$wdc->set_user('forpdfsending');
+$wdc->set_pass('');
+// use HTTP/1.1
+$wdc->set_protocol(1);
+// enable debugging
+$wdc->set_debug(false);
+
+
+if (!$wdc->open()) {
+  print 'Error: could not open server connection';
+  exit;
+}
+
+// check if server supports webdav rfc 2518
+if (!$wdc->check_webdav()) {
+  print 'Error: server does not support webdav or user/password may be wrong';
+  exit;
+}
+
+$http_status  = $wdc->mkcol("/backups");
+
+$http_status = $wdc->put_file("/backups/"."test.zip", $backupFileName);
+print 'webdav server returns ' . $http_status;
+
+$wdc->close();
+flush();
+?>
